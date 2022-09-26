c6ceaf9a46f53e932e583795764328a0486000dd
ValeJappo <cioffval@gmail.com>
2022-09-26 20:47:41 +0200

    minor fix

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ function getSettings(req, name){
 }
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', authorizedHosts);
+    if(checkHost(req))
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
